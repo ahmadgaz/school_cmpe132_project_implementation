748c3b8ad26de3b74f06e0fe53bce505b7ce8a5b
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2023-11-25 14:33:03 -0800

    Initial Commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import React from 'react';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -17,19 +21,30 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  const [loadingPathname, setLoadingPathname] = React.useState('');
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+        const pathnameLoaded = pathname === link.href;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            onClick={() => setLoadingPathname(link.href)}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium transition-all duration-100 ease-in-out hover:bg-sky-100 hover:text-blue-600 active:text-blue-300 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'animate-pulse bg-sky-50 text-blue-300':
+                  loadingPathname === link.href && !pathnameLoaded,
+                'bg-sky-100 text-blue-600': pathnameLoaded,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'block' });
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'block',
+});
