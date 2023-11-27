8c013d39e868311ca5cc13f7f192d1c35f989271
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2023-11-26 23:41:30 -0800

    Refactor code to use RevenueType and FormattedCustomersTableType

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import { FormattedCustomersTableType } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
 }: {
-  customers: FormattedCustomersTable[];
+  customers: FormattedCustomersTableType[];
 }) {
   return (
     <div className="w-full">
